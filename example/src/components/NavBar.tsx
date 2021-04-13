77bd96c6fece23c987fdd36910e8a9e095b887d4
Vladimir Breusov <breusov.vova@gmail.com>
2021-04-13 19:48:21 +0300

    refactor: navbar

--- example/src/components/NavBar.tsx
+++ example/src/components/NavBar.tsx
@@ -1,36 +1,27 @@
-import React from "react";
+import React, {CSSProperties} from "react";
 import {Link} from "react-router-dom";
 import routes from "../routes";
 
+const styles: CSSProperties = {
+    marginBottom: "1rem",
+    display: "flex",
+    flexDirection: "column",
+};
+
 const NavBar = () => {
     return (
-        <nav style={{marginBottom: "1rem"}}>
+        <nav style={styles}>
             <h2>Nav Bar</h2>
-
-            <div>
-                <Link to={routes.main.path()}>{routes.main.title}</Link>
-            </div>
-            <div>
-                <Link to={routes.second.path()}>{routes.second.title}</Link>
-            </div>
-            <div>
-                <Link to={routes.withParameter.path(1)}>
-                    {routes.withParameter.title} 1
-                </Link>
-            </div>
-            <div>
-                <Link to={routes.withParameter.path(2)}>
-                    {routes.withParameter.title} 2
-                </Link>
-            </div>
-
-            <div>
-                <Link to="/404">404 page</Link>
-            </div>
-
-            <div>
-                <Link to="/404-not-found">404 page with redirect</Link>
-            </div>
+            <Link to={routes.main.path()}>{routes.main.title}</Link>
+            <Link to={routes.second.path()}>{routes.second.title}</Link>
+            <Link to={routes.withParameter.path(1)}>
+                {routes.withParameter.title} 1
+            </Link>
+            <Link to={routes.withParameter.path(2)}>
+                {routes.withParameter.title} 2
+            </Link>
+            <Link to="/404">404 page</Link>
+            <Link to="/404-not-found">404 page with redirect</Link>
         </nav>
     );
 };
